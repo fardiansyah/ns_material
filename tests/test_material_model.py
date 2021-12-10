8c43b6231dee751b80d2df62a37539989342d81e
fardiansyah <fardiansyah87@gmail.com>
2021-12-10 20:02:58 +0000

    fix: imports

--- tests/test_material_model.py
+++ tests/test_material_model.py
@@ -1,5 +1,5 @@
-from odoo.tests.common import SavepointCase, TransactionCase
-from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
 from odoo.tests import tagged
 
 @tagged('post_install', '-at_install', 'ns_material')
